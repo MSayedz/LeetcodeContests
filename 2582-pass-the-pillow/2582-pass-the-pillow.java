cf9c988ab28b13b1dc2ab8802033496c7065c03c
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-03-05 14:05:52 +0100

    Time: 0 ms (100.00%), Space: 39.4 MB (100.00%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int rounds = time/(n-1); 
+        int k = time % (n-1);
+        return rounds % 2 == 0 ? k+1 : n-k;
+    }
+}
