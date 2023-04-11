a9da956eee281ce0c062b1a59a69e294f8d53e5b
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-11 18:02:44 +0200

    Time: 429 ms (12.01%), Space: 43.9 MB (7.09%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -10,10 +10,10 @@ class Solution {
         if (memo[i] != 0) {
             return memo[i] == 1 ? true : false;
         }
-
         
-       int furthestJump = Math.min(i + nums[i], nums.length - 1);
-        for (int j = i + 1; j <= furthestJump; j++) {
+        int max = Math.min(i + nums[i], nums.length - 1);
+        
+        for (int j = i + 1; j <= max; j++) {
             if (canJump(nums, j, memo)) {
                 memo[i] = 1;
                 return true;
