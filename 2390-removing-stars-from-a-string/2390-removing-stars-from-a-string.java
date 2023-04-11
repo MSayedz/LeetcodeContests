aeb507456d731c717aead873bc323cd456068599
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-11 16:24:27 +0200

    Time: 14 ms (98.94%), Space: 43.4 MB (84.49%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,5 +1,15 @@
 class Solution {
     public String removeStars(String s) {
+        char[] res = s.toCharArray();
+        int j = 0;
+        for(int i = 0; i < res.length; i++, j++){
+            res[j] = res[i];
+            if(j > 0 && res[j] == '*') {
+                j -= 2;
+            }
+        }
+        return new String(res, 0, j);
+        
 //         Deque<Character> deque = new ArrayDeque<>();
         
 //         for(int i=0;i<s.length();i++){
@@ -11,14 +21,12 @@ class Solution {
         
 //         return deque.stream().collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
         
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray())
-            if (c == '*')
-                sb.setLength(sb.length() - 1);// sb.delectCharAt(sb.length() - 1);
-            else
-                sb.append(c);
-        return sb.toString();
-        
-        
+        // StringBuilder sb = new StringBuilder();
+        // for (char c : s.toCharArray())
+        //     if (c == '*')
+        //         sb.setLength(sb.length() - 1);// sb.delectCharAt(sb.length() - 1);
+        //     else
+        //         sb.append(c);
+        // return sb.toString();
     }
 }
