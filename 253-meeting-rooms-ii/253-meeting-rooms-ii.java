07aa9233b9bedaddd9a8c28eb661a299f987d677
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-15 15:48:11 +0200

    Time: 3 ms (99.05%), Space: 42.3 MB (70.34%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+
+        
+        for(int i=0;i<intervals.length;i++){
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+        
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        
+        int count = 0;
+        int res = 0;
+        for(int i=0,j=0;i<intervals.length;i++){
+            count ++;
+            if(starts[i]>=ends[j]){
+                j++;
+                count --;
+            }
+            res = Math.max(res, count);
+        }
+        
+        return Math.max(res, count);
+    }
+}
