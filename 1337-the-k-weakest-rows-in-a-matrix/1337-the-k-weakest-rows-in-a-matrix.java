3cda5795fd4d042b481ff741f68f3a7433eff52a
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-15 15:07:18 +0200

    Time: 3 ms (26.64%), Space: 43.8 MB (32.97%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -11,26 +11,16 @@ class Solution {
                     count++;
                 }
             }
-            System.out.println(i +" "+ count);
             map.put(i,count);
         }
-        
-        map.values().stream().forEach(System.out::println);
         
         PriorityQueue<Map.Entry<Integer,Integer>> maxHeap = new PriorityQueue<>((a,b) -> b.getValue()==a.getValue() ? b.getKey()-a.getKey(): b.getValue()-a.getValue());
         
         maxHeap.addAll(map.entrySet());
         
-        System.out.println("---------" + maxHeap.size());
-        
-        maxHeap.stream().map(c->c.getValue()).forEach(System.out::println);
         
         while(maxHeap.size()>k)
             maxHeap.poll();
-        System.out.println("---------" + maxHeap.size());
-        
-        
-        maxHeap.stream().map(c->c.getValue()).forEach(System.out::println);
         int[] res = new int[k];
         for(int i=k-1;i>-1;i--)
             res[i] = maxHeap.poll().getKey();
