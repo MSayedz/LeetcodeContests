9a0c6ac1c01bced46abb24bb8f3d76dd085dd630
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-17 00:50:59 +0200

    Time: 8 ms (16.68%), Space: 50.5 MB (84.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,26 +1,45 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int numberOfIslands = 0;
+    class Cell {
+        int row; int col;
         
+        public Cell(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+        
+        public int getRow(){return this.row;}
+        public int getCol(){return this.col;}
+    }
+    int[] DIR = new int[]{0, 1, 0, -1, 0};
+    
+    public int numIslands(char[][] grid) {
+        int numOfIslands =0;
+        
+        Queue<Cell> queue = new LinkedList<>();
         for(int i=0;i<grid.length;i++)
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == '1'){
-                    numberOfIslands++;
-                    dfs(grid, i, j);
+                    numOfIslands ++;
+                    queue.offer(new Cell(i,j));
+                    grid[i][j] = '0';
+                    
+                    while(!queue.isEmpty()){
+                        Cell cur = queue.poll();
+                        
+                        for(int k=0; k<4;k++){
+                            int nr = cur.getRow() + DIR[k];
+                            int nc = cur.getCol() + DIR[k+1];
+                            if(nr < 0 || nc < 0 
+                               || nr>grid.length-1 || nc>grid[0].length-1
+                              || grid[nr][nc] == '0')
+                                continue;
+                            grid[nr][nc] = '0';
+                            queue.offer(new Cell(nr, nc));
+                        }
+                    }
                 }
             }
         
-        return numberOfIslands;
-    }
-    
-    private void dfs(char[][] grid, int i, int j){
-        if(i<0 || j<0 || j>grid[0].length-1 || i>grid.length-1 || grid[i][j] == '0')
-            return;
-        
-        grid[i][j] = '0';
-        dfs(grid, i-1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j+1);
+        return numOfIslands;
     }
 }
