e276cdf2733a4b8d43aaa46e3a69ea3f37ed91ec
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-16 14:22:57 +0200

    Time: 36 ms (86.48%), Space: 49.7 MB (73.84%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length - 2;i++){
             if (i == 0 || nums[i] != nums[i - 1]) {
                 int l = i+1;
                 int r = nums.length-1;
