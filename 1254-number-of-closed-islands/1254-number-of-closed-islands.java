4a3423194aec98e26f109c4f237d99aa0a74a237
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-05-02 22:09:43 +0300

    Time: 2 ms (84.52%), Space: 43.1 MB (54.23%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,10 +1,9 @@
 class Solution {
     public int closedIsland(int[][] grid) {
         int count = 0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
         for(int i=0;i<grid.length;i++)
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == 0 && !visited[i][j] && dfs(grid, visited, i, j)){
+                if(grid[i][j] == 0 && dfs(grid, i, j)){
                     count ++;
                 }
             }
@@ -12,27 +11,13 @@ class Solution {
         return count;
     }
     
-    static boolean dfs(int[][] grid, boolean[][] visited, int i, int j){
-        if(i<0 || j<0 || i>grid.length-1 || j>grid[0].length-1){
+    static boolean dfs(int[][] grid, int i, int j){
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
             return false;
-        }
-        if (grid[i][j] == 1 || visited[i][j]) {
+        if (grid[i][j] == 1)
             return true;
-        }
-        
-        visited[i][j] = true;
-        boolean isClosed = true;
-        
-        int[] DIR =  new int[] {0, 1, 0, -1, 0};
-        
-        for(int k=0;k<4;k++){
-            int r = i + DIR[k];
-            int c = j + DIR[k+1];
-            
-            if(!dfs(grid, visited, r, c)){
-                isClosed = false;
-            }
-        }
-        return isClosed;
+        grid[i][j] = 1;
+        return dfs(grid, i - 1, j) & dfs(grid, i, j - 1) 
+            & dfs(grid, i + 1, j) & dfs(grid, i, j + 1);
     }
 }
