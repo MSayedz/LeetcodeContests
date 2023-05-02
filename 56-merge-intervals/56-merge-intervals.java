eb7066f2d2477194f6c42905627ed8c21e6c1b9d
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-05-02 23:52:15 +0300

    Time: 8 ms (96.96%), Space: 47.6 MB (38.47%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -2,24 +2,33 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         List<int[]> resList = new ArrayList<>();
-        int start = 0; 
-        int end = 0;
-        for(int i=0;i<intervals.length;i++){
-            if(i == 0){
-                start = intervals[i][0];
-                end = intervals[i][1];
-            }else{
-                if(end >= intervals[i][0]){
-                    end = Math.max(end, intervals[i][1]);
-                } else{
-                    resList.add(new int[] {start, end});
-                    start = intervals[i][0];
-                    end = intervals[i][1];
-                }
-            }
+        // int start = 0; 
+        // int end = 0;
+        // for(int i=0;i<intervals.length;i++){
+        //     if(i == 0){
+        //         start = intervals[i][0];
+        //         end = intervals[i][1];
+        //     }else{
+        //         if(end >= intervals[i][0]){
+        //             end = Math.max(end, intervals[i][1]);
+        //         } else{
+        //             resList.add(new int[] {start, end});
+        //             start = intervals[i][0];
+        //             end = intervals[i][1];
+        //         }
+        //     }
+        //     resList.add(new int[] {start, end});
+        // }
+        
+        for(int i = 0; i < intervals.length; i++){
+            int[] tmp = intervals[i];
+            while(i < intervals.length && intervals[i][0] <= tmp[1])
+                tmp[1] = Math.max(tmp[1], intervals[i++][1]);
+            
+            resList.add(tmp);
+            i--;
         }
         
-        resList.add(new int[] {start, end});
         
         return resList.toArray(new int[0][0]);
     }
